6d3b85ec5698b778e612e977ba925f37cd556604
Pierre Olivier <pierreolivierpaquet@hotmail.com>
2024-06-22 16:08:44 -0400

    TBD [ function that broadcast the messages to all the clients ]

--- mini_serv.c
+++ mini_serv.c
@@ -6,7 +6,7 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 21:13:12 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/06/20 13:18:05 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/06/22 16:07:38 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/select.h> // select(); fd_set -> ubuntu
 #include <netinet/in.h>	// sockaddr_in -> ubuntu
 #include <stdbool.h>
 
 /// @brief Represents a client for the server.
 typedef struct	s_client
 {
-	int					fd;
+	int					id;
 	struct sockaddr_in	client_address;
-	char				buffer[BUFSIZ];
+	char				buffer[1024];
 }						t_client;
 
 typedef	struct	s_server
@@ -36,6 +37,9 @@ typedef	struct	s_server
 	fd_set				read_set;			//	Set of file descriptors that have a reading status.
 	fd_set				write_set;			//	Set of file descriptors that are ready to be written.
 	int					highest_fd;			// Amount of clients within the server.
+	int					client_id;			// Client identification.
+	char				send_buf[BUFSIZ];
+	char				receive_buf[BUFSIZ];
 }						t_server;
 
 int	ft_putstring_fd(char *string, int fd)
@@ -64,8 +68,6 @@ void	get_server(t_server **get)
 	return ;
 }
 
-
-
 int	mini_serv(void)
 {
 	t_server			*server;
@@ -73,10 +75,14 @@ int	mini_serv(void)
 	int					new_client_fd;
 	struct sockaddr_in	client_address;
 	socklen_t			sock_lenght;
+	int					bytes_received;
+	int					j;
+	int					k;
 
 	server = NULL;
 	get_server(&server);
 
+	printf("ENTERING THE INFINTE LOOP\n");
 	while (true)
 	{
 //		select() function is destructive, so we need to make a copy. current_set serves as a backup of all client file descriptors.
@@ -90,24 +96,71 @@ int	mini_serv(void)
 					&server->write_set,		\
 					NULL,					\
 					NULL) < 0)
+		{
 			continue; // If select() encounters an error, the program must not quit; Instead, it is going to retry to examine the file descriptor(s).
+		}
 
 		i = server->fd;
 		while (i <= server->highest_fd) // Iterating through all the server file descriptor(s).
 		{
 			if(FD_ISSET(i, &server->read_set)) // If the 'i' fd is ready for reading.
+			{
+				if (i == server->fd) // Means that a new client connection is required.
 				{
-					if (i == server->fd) // means that a new client connection is required.
+					new_client_fd = 0;
+					bzero(&client_address, sizeof(client_address));
+					sock_lenght = 0;
+					new_client_fd = accept(server->fd, \
+											(struct sockaddr *)&client_address, \
+											&sock_lenght);
+					if (new_client_fd > 0)
 					{
-						new_client_fd = 0;
-						bzero(&client_address, sizeof(client_address));
-						sock_lenght = 0;
-						new_client_fd = accept(server->fd, \
-												(struct sockaddr *)&client_address, \
-												&sock_lenght);
+						if (new_client_fd > server->highest_fd)
+							server->highest_fd = new_client_fd;
+						server->clients[new_client_fd].id = server->client_id;
+						server->client_id++;
+						FD_SET(new_client_fd, &server->current_set);		// Adds the new client file descriptor to the monitored set.
+						sprintf(server->send_buf, "server: client %d just arrived\n", server->clients[new_client_fd].id);
+						printf("%s", server->send_buf);
 					}
-					
+					else
+						continue;
 				}
+				else // Means that a message is sent.
+				{
+					bytes_received = recv(i, server->receive_buf, sizeof(server->receive_buf), 0);
+					if (bytes_received <= 0)
+					{
+						sprintf(server->send_buf, "server: client %d just left\n", server->clients[i].id);
+						// ADD FUNCTION THAT SENDS TO EVERY CLIENT
+						printf("%s", server->send_buf);
+						FD_CLR(i, &server->current_set); // Deletes the file descriptor from the monitored set.
+						close(i); // Closes the file descriptor.
+						bzero(&server->clients[i], sizeof(server->clients[i])); // Deletes the data stored to the client struct.
+					}
+					else
+					{
+						j = 0; // To parse the received message.
+						k = strlen(server->clients[i].buffer); // To navigate into the client message buffer.
+						while (j < bytes_received)
+						{
+							server->clients[i].buffer[k] = server->receive_buf[j]; // Copies the content of the received buffer to the client's message.
+							if (server->clients[i].buffer[k] == '\n')
+							{
+								server->clients[i].buffer[k] = '\0';
+								sprintf(server->send_buf, "client %d: %s\n", server->clients[i].id, server->clients[i].buffer);
+								// ADD FUNCTION THAT SENDS TO EVERY CLIENT
+								printf("%s", server->send_buf);
+								bzero(server->clients[i].buffer, strlen(server->clients[i].buffer));
+								k = -1;
+							}
+							j++;
+							k++;
+						}
+					}
+				}
+				break;
+			}
 			i++;
 		}
 
@@ -160,9 +213,3 @@ int	main(int argc, char **argv)
 
 	return (EXIT_SUCCESS);
 }
-
-// [X] Check validity of arguments.
-// [X] Create a socket (sockaddr_in) for the server itself.
-// [X] Bind the socket to network.
-// [X] listen for incoming connetions.
-// [] accept incoming connections. 
